99d4b10fe30dbc30e9aba722c8a37a26e126e237
kuileta783 <kuileta@hotmail.com>
2025-09-08 10:37:19 +0800

    chore: 切换 GA 测量 ID 为数字0版本 G-5J06YYPWHL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ import ClientBody from "./ClientBody";
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>Hostingtigger | Best Web Hosting Reviews</title>
-          <GoogleAnalytics measurementId="G-5JO6YYPWHL" />
+          <GoogleAnalytics measurementId="G-5J06YYPWHL" />
         </head>
         <body>
           <ClientBody>{children}</ClientBody>
